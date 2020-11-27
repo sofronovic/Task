53ff6df656817247671bed16c325904300f8ce5e
nikola <sofronovicn@gmail.com>
2020-11-27 21:27:09 +0100

    Navigation component

--- app/src/main/java/com/nsofronovic/task/ui/postdetails/PostDetailsFragment.kt
+++ app/src/main/java/com/nsofronovic/task/ui/postdetails/PostDetailsFragment.kt
@@ -1,24 +1,19 @@
-package com.nsofronovic.task.ui.post
+package com.nsofronovic.task.ui.postdetails
 
 import android.os.Bundle
-import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import com.nsofronovic.task.R
-import io.reactivex.Observable
-import io.reactivex.Observer
-import java.util.concurrent.TimeUnit
 
-class PostFragment : Fragment() {
+class PostDetailsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = layoutInflater.inflate(R.layout.fragment_post, container, false)
+        val view = layoutInflater.inflate(R.layout.fragment_post_details, container, false)
 
         return view
     }
